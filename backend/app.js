9a005bfc7fd5e76c4ae7d1478435a02d2633da8d
SEEDART007 <chakrabortysiddhartha58@gmail.com>
2026-01-17 22:19:52 +0530

    added many prior backend sanitization

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,33 @@
 const express = require('express')
 const app = express();
 const cors = require('cors')
+const xss = require('xss-clean')
+const sqlSanitizer = require('express-mongo-sanitize')
+const rateLimit = require('express-rate-limit')
 const authRouter = require('./routes/authRoute')
 const blogRouter = require('./routes/blogRoute')
+const helmet = require('helmet');
 
 
 app.use(express.json())
 
+app.use(helmet())
+
+app.use(sqlSanitizer())
+
 app.use(cors());
 
-app.use("/api/v1",authRouter)
+const limiter = rateLimit({
+    max:100,
+    windowMs : 60*60*1000,
+    message:'too many request!!try again later'
+})
+app.use('/api/v1',limiter)
+app.use(xss());
+
+
+
+app.use("/api/v1/auth",authRouter)
 
 app.use("/api/v1/blogs",blogRouter)
 
--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 // Base URL of your backend
 const api = axios.create({
-  baseURL: "http://localhost:3000/api/v1", // replace with your backend URL
+  baseURL: "http://localhost:3000/api/v1/auth", // replace with your backend URL
 });
 
 // Automatically attach JWT token from localStorage
